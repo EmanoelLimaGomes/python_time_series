81e0f0a4047efa35a65d07a527aa7ab202c44aa0
BluePen <160792844+EmanoelLimaGomes@users.noreply.github.com>
2025-08-25 20:28:11 -0300

    Create cadeira_preta.py

---
+++ cadeira_preta.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Cabeçalho: infos técnicas ou metadados do arquivo (autor, data, etc.)
+
+"""
+Docstring do módulo: explica o que o arquivo faz.
+"""
+
+def soma(a, b):
+    """
+    Docstring da função: explica o que ela faz,
+    seus parâmetros e retorno.
+    """
+    return a + b
+
+# Diferença:
+# - Cabeçalho = comentários no topo (informações externas/gerais)
+# - Docstring = string com """ usada p/ documentar módulos, classes e funções
